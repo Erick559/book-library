d60f308f98a42120a31c7f57d7b629db5935e015
Erick559 <erickungu82@gmail.com>
2024-03-14 12:27:33 +0300

    feat: Finish logic for displaying books.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const myLibrary = [];
+const myLibrary = [{title:'48 Laws of Power', author:'Robert Greene', pageSize:496, genre:'Self Help', readStatus:'Started Reading'},
+{title:'The Hobbit', author:'JRR Tolkien', pageSize:320, genre:'Fiction', readStatus:'Not Yet'},];
 
 const addButton = document.querySelector('.add-button');
 const closeModalButton = document.querySelector('.close')
 const modalAddButton = document.querySelector('.add');
+const booksContainer = document.querySelector('.books-container');
 
 
 function Book(title,author,pageSize,genre,readStatus){
@@ -11,7 +13,7 @@ function Book(title,author,pageSize,genre,readStatus){
     this.pageSize = pageSize;
     this.genre = genre;
     this.readStatus = readStatus;
-}
+};
 
 function addBookToLibrary(){
     const title = document.querySelector('#title').value;
@@ -33,7 +35,7 @@ function addBookToLibrary(){
 
         console.log(myLibrary);
     }
-}
+};
 
 function openModal(){
     const overlay = document.querySelector('.overlay');
@@ -41,7 +43,7 @@ function openModal(){
 
     modal.classList.add('active');
     overlay.classList.add('active');
-}
+};
 
 function closeModal(){
     const overlay = document.querySelector('.overlay');
@@ -54,8 +56,47 @@ function closeModal(){
 
     modal.classList.remove('active');
     overlay.classList.remove('active');
-}
+};
 
+function displayBooks(){
+    booksContainer.textContent=''
+
+    myLibrary.forEach(book => {
+        const card = document.createElement('div');
+        card.classList.add('book-card')
+
+        const title = document.createElement('h2');
+        const author = document.createElement('h2')
+        const genre = document.createElement('h2');
+        const numberOfPages = document.createElement('h2');
+        const readStatus = document.createElement('button');
+        const removeButton = document.createElement('button');
+        const buttonsDiv = document.createElement('div');
+        const infoDiv = document.createElement('div');
+
+
+        title.textContent = `Book Title: ${book.title}`;
+        author.textContent = `Author: ${book.author}`;
+        genre.textContent = `Genre: ${book.genre}`;
+        numberOfPages.textContent =`Number of Pages: ${book.pageSize}`;
+        readStatus.textContent = book.readStatus;
+        removeButton.textContent = 'Remove';
+        readStatus.classList.add('status');
+        buttonsDiv.classList.add('button-div');
+        removeButton.classList.add('remove-button');
+        infoDiv.classList.add('info-div');
+
+        infoDiv.append(title, author, genre, numberOfPages)
+        buttonsDiv.append(readStatus,removeButton)
+
+        card.append(infoDiv,buttonsDiv);
+
+        booksContainer.appendChild(card)
+    })
+};
+
+
+window.addEventListener('load', displayBooks)
 
 addButton.addEventListener('click', openModal);
 
@@ -64,7 +105,10 @@ closeModalButton.addEventListener('click', closeModal)
 modalAddButton.addEventListener('click', ()=>{
     addBookToLibrary();
     closeModal();
+    displayBooks();
 })
+
+
 
 
 
