ad382ba741716597b878ec040798d85fbce2913d
Erick559 <erickungu82@gmail.com>
2024-03-14 10:13:01 +0300

    feat: Complete adding newly created book object to the array.

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
+const myLibrary = [];
+
 const addButton = document.querySelector('.add-button');
 const closeModalButton = document.querySelector('.close')
+const modalAddButton = document.querySelector('.add');
 
-addButton.addEventListener('click', () => {
+
+function Book(title,author,pageSize,genre,readStatus){
+    this.title = title;
+    this.author = author;
+    this.pageSize = pageSize;
+    this.genre = genre;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary(){
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pageSize = document.querySelector('#page-size').value;
+    const genre = document.querySelector('#genre').value;
+    const readStatus = document.querySelector('#read-status').value;
+
+    const book = new Book(title, author, pageSize, genre, readStatus);
+    myLibrary.push(book);
+
+    console.log(myLibrary);
+}
+
+function openModal(){
     const overlay = document.querySelector('.overlay');
     const modal = document.querySelector('.form-modal');
 
     modal.classList.add('active');
     overlay.classList.add('active');
-})
+}
 
-closeModalButton.addEventListener('click', () =>{
+function closeModal(){
     const overlay = document.querySelector('.overlay');
     const modal = document.querySelector('.form-modal');
+    const inputs = document.querySelectorAll('input');
+
+    inputs.forEach(input => {
+        input.value = '';
+    })
 
     modal.classList.remove('active');
     overlay.classList.remove('active');
+}
+
+
+addButton.addEventListener('click', openModal);
+
+closeModalButton.addEventListener('click', closeModal)
+
+modalAddButton.addEventListener('click', ()=>{
+    addBookToLibrary();
+    closeModal();
 })
+
+
+
+
 
